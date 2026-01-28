7da3be483f85ad0ab6afc81288daaef062cb8a16
charley-2333 <charleyalexandrea17@gmail.com>
2026-01-28 16:48:47 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,202 @@
+const questions = [
+  {
+    q: "Management is best defined as:",
+    options: [
+      "Getting work done by people",
+      "Planning and controlling business activities",
+      "Getting things done through people efficiently and effectively",
+      "Organizing business resources"
+    ],
+    answer: "Getting things done through people efficiently and effectively"
+  },
+  {
+    q: "Who is regarded as the father of scientific management?",
+    options: [
+      "Henri Fayol",
+      "Peter Drucker",
+      "Frederick Taylor",
+      "Max Weber"
+    ],
+    answer: "Frederick Taylor"
+  },
+  {
+    q: "Which of the following is NOT a function of management?",
+    options: [
+      "Planning",
+      "Organizing",
+      "Staffing",
+      "Advertising"
+    ],
+    answer: "Advertising"
+  },
+  {
+    q: "The management function that involves setting objectives is called:",
+    options: [
+      "Planning",
+      "Directing",
+      "Controlling",
+      "Coordinating"
+    ],
+    answer: "Planning"
+  },
+  {
+    q: "Unity of command means:",
+    options: [
+      "One manager controls many workers",
+      "One worker receives orders from one superior",
+      "Managers work together",
+      "Authority equals responsibility"
+    ],
+    answer: "One worker receives orders from one superior"
+  },
+  {
+    q: "Which of the following is NOT a factor of production?",
+    options: [
+      "Land",
+      "Labour",
+      "Capital",
+      "Management style"
+    ],
+    answer: "Management style"
+  },
+  {
+    q: "The primary objective of business is to:",
+    options: [
+      "Provide employment",
+      "Make profit",
+      "Satisfy government",
+      "Increase competition"
+    ],
+    answer: "Make profit"
+  },
+  {
+    q: "The process of arranging tasks and resources is known as:",
+    options: [
+      "Planning",
+      "Organizing",
+      "Staffing",
+      "Controlling"
+    ],
+    answer: "Organizing"
+  },
+  {
+    q: "Which theory emphasizes rules and procedures?",
+    options: [
+      "Human relations theory",
+      "Scientific management",
+      "Bureaucratic theory",
+      "Systems theory"
+    ],
+    answer: "Bureaucratic theory"
+  },
+  {
+    q: "Span of control refers to:",
+    options: [
+      "Number of departments",
+      "Number of managers",
+      "Number of subordinates under a manager",
+      "Level of authority"
+    ],
+    answer: "Number of subordinates under a manager"
+  },
+  {
+    q: "Motivation is best described as:",
+    options: [
+      "Payment of salaries",
+      "Punishment of workers",
+      "Process of stimulating workers to perform",
+      "Promotion of staff"
+    ],
+    answer: "Process of stimulating workers to perform"
+  },
+  {
+    q: "Which of the following is a feature of management?",
+    options: [
+      "Static",
+      "Tangible",
+      "Universal",
+      "Simple"
+    ],
+    answer: "Universal"
+  },
+  {
+    q: "Decision making is mostly associated with which management function?",
+    options: [
+      "Staffing",
+      "Planning",
+      "Directing",
+      "Controlling"
+    ],
+    answer: "Planning"
+  },
+  {
+    q: "Which is an internal business environment factor?",
+    options: [
+      "Government policy",
+      "Technology",
+      "Employees",
+      "Economy"
+    ],
+    answer: "Employees"
+  },
+  {
+    q: "Authority without responsibility leads to:",
+    options: [
+      "Discipline",
+      "Efficiency",
+      "Irresponsibility",
+      "Accountability"
+    ],
+    answer: "Irresponsibility"
+  },
+  {
+    q: "Management is said to be a process because it:",
+    options: [
+      "Is continuous",
+      "Has authority",
+      "Uses resources",
+      "Is profitable"
+    ],
+    answer: "Is continuous"
+  },
+  {
+    q: "Which of these is a characteristic of a good manager?",
+    options: [
+      "Laziness",
+      "Dictatorship",
+      "Leadership",
+      "Favoritism"
+    ],
+    answer: "Leadership"
+  },
+  {
+    q: "The final stage of the management process is:",
+    options: [
+      "Planning",
+      "Organizing",
+      "Directing",
+      "Controlling"
+    ],
+    answer: "Controlling"
+  },
+  {
+    q: "Which level of management deals with policy formulation?",
+    options: [
+      "Lower level",
+      "Middle level",
+      "Top level",
+      "Supervisory level"
+    ],
+    answer: "Top level"
+  },
+  {
+    q: "Management by Objectives (MBO) was popularized by:",
+    options: [
+      "Henri Fayol",
+      "Peter Drucker",
+      "Frederick Taylor",
+      "Elton Mayo"
+    ],
+    answer: "Peter Drucker"
+  }
+];
